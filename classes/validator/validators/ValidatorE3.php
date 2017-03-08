6953084e22798851253c531e059dda44bbfeab69
Dennis D <dennis@double-d.it>
2017-03-08 13:54:49 +0100

    Update ValidatorE3.php

--- classes/validator/validators/ValidatorE3.php
+++ classes/validator/validators/ValidatorE3.php
@@ -19,7 +19,7 @@ namespace malkusch\bav;
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-class ValidatorE3 extends WeightedIterationValidator
+class ValidatorE3 extends ValidatorChain
 {
     public function __construct(Bank $bank)
     {
